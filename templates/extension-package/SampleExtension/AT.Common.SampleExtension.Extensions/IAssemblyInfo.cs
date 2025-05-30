db98cf4bd7028d3d8b6c9602064c9ea4b75837cd
Bjørnar Prytz <155627187+at-bgp@users.noreply.github.com>
2025-05-30 10:35:20 +0200

    Liten opprydning i ArchUnit-testene (#35)
    - Fjernet en unødvendig test
    - Fikset prosjektnavnet SamplePackage->SampleExtensions
    - Fikset den siste build-warning (XML-comments)
    
    ---------
    
    Co-authored-by: Bjørnar Prytz <bjornar.groholt.prytz@arbeidstilsynet.no>

--- templates/extension-package/SampleExtension/AT.Common.SampleExtension.Extensions/IAssemblyInfo.cs
+++ templates/extension-package/SampleExtension/AT.Common.SampleExtension.Extensions/IAssemblyInfo.cs
@@ -2,4 +2,7 @@ using System.Runtime.CompilerServices;
 
 namespace Arbeidstilsynet.Common.SampleExtension.Extensions;
 
+/// <summary>
+/// Marker interface for assembly information.
+/// </summary>
 public interface IAssemblyInfo { }
