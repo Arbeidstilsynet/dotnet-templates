f552553e1995aebf2755157641a97038b1d100b6
Bjørnar Prytz <bjornar.groholt.prytz@arbeidstilsynet.no>
2025-06-03 11:46:59 +0200

    Swapped out boilerplate configuration methods for AT.Common.AspNetCore.Extensions

--- templates/hexarch/API/API.Adapters/Program.cs
+++ templates/hexarch/API/API.Adapters/Program.cs
@@ -1,8 +1,10 @@
-using Arbeidstilsynet.HexagonalArchitectureTemplateDocker.API.Adapters;
+using System.Net;
+using Arbeidstilsynet.Common.AspNetCore.Extensions;
 using Arbeidstilsynet.HexagonalArchitectureTemplateDocker.API.Adapters.Api;
-using Arbeidstilsynet.HexagonalArchitectureTemplateDocker.API.Adapters.Extensions;
+using Arbeidstilsynet.HexagonalArchitectureTemplateDocker.API.Ports;
 using Arbeidstilsynet.HexagonalArchitectureTemplateDocker.Domain.Logic.DependencyInjection;
 using Arbeidstilsynet.HexagonalArchitectureTemplateDocker.Infrastructure.Adapters.DependencyInjection;
+using IAssemblyInfo = Arbeidstilsynet.HexagonalArchitectureTemplateDocker.API.Adapters.IAssemblyInfo;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,10 +12,8 @@ var appSettings = builder.Configuration.GetRequired<AppSettings>();
 var services = builder.Services;
 var env = builder.Environment;
 
-services.ConfigureApi();
-services.ConfigureSwagger();
-services.ConfigureLogging(env);
-services.ConfigureOpenTelemetry(IAssemblyInfo.AppName);
+services.ConfigureApi(
+    IAssemblyInfo.AppName, env);
 
 services.AddDomain();
 services.AddInfrastructureServices(appSettings.DatabaseConfiguration);
@@ -25,8 +25,6 @@ if (env.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
-app.AddApi();
-
-app.AddScalar();
+app.AddApi(options => options.AddExceptionMapping<SakNotFoundException>(HttpStatusCode.NotFound));
 
 await app.RunAsync();
