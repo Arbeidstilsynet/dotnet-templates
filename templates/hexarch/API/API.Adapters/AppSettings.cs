221b615d4558365e86030f5ffc56de5a555aa47e
vaernion <55745090+vaernion@users.noreply.github.com>
2025-06-12 14:40:33 +0200

    feat(hexarch): add cors support (#49)
    - Added CORS support
    - Removed unused package in root project
    - Changed Grafana port to 4000 to avoid conflicts
    
    related: https://github.com/Arbeidstilsynet/dotnet-common/pull/50

--- templates/hexarch/API/API.Adapters/AppSettings.cs
+++ templates/hexarch/API/API.Adapters/AppSettings.cs
@@ -8,4 +8,13 @@ internal record AppSettings
     [Required]
     [ConfigurationKeyName("DatabaseConfiguration")]
     public required DatabaseConfiguration DatabaseConfiguration { get; init; }
+
+    [ConfigurationKeyName("CorsConfiguration")]
+    public CorsConfiguration CorsConfiguration { get; init; } = new();
+}
+
+internal record CorsConfiguration
+{
+    public string[] AllowedOrigins { get; init; } = [];
+    public bool AllowCredentials { get; init; } = false;
 }
