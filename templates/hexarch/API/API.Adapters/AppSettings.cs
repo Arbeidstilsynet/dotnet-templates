d3c240b9c8ea0f0835cd5be7bf290500a5f8b335
Bjørnar Prytz <bjornar.groholt.prytz@arbeidstilsynet.no>
2025-06-13 09:47:34 +0200

    Fjerne ubrukte dependencies

--- templates/hexarch/API/API.Adapters/AppSettings.cs
+++ templates/hexarch/API/API.Adapters/AppSettings.cs
@@ -7,12 +7,30 @@ namespace Arbeidstilsynet.HexagonalArchitectureTemplateDocker.API.Adapters.Api;
 internal record AppSettings
 {
     [Required]
+    [ConfigurationKeyName("API")]
+    public required ApiConfiguration ApiConfig { get; init; }
+    
+    
+    [Required]
     [ConfigurationKeyName("Infrastructure")]
     public required InfrastructureConfiguration InfrastructureConfig { get; init; }
     
     [Required]
     [ConfigurationKeyName("Domain")]
     public required DomainConfiguration DomainConfig { get; init; }
+}
+
+internal record ApiConfiguration
+{
+    [ConfigurationKeyName("Cors")]
+    public CorsConfiguration Cors { get; init; } = new ();
+}
+
+internal record CorsConfiguration
+{
+    [Required]
+    public string[] AllowedOrigins { get; init; } = [];
     
-    
+    [Required]
+    public bool AllowCredentials { get; init; } = false;
 }
