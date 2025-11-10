b44a882e089f59900bc5addffc4ba90c76faee69
Maurice Kilgus <maurice.kilgus@arbeidstilsynet.no>
2025-11-10 07:09:50 +0100

    chore: add dotnet ef migration (#107)

---
+++ templates/hexarch/Infrastructure/Infrastructure.Adapters/Db/DatabaseMigrationService.cs
@@ -0,0 +1,18 @@
+using Arbeidstilsynet.HexagonalArchitectureTemplateDocker.Infrastructure.Ports;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Arbeidstilsynet.HexagonalArchitectureTemplateDocker.Infrastructure.Adapters.Db;
+
+internal class DatabaseMigrationService(
+    SakDbContext dbContext,
+    ILogger<DatabaseMigrationService> logger
+) : IDatabaseMigrationService
+{
+    public async Task RunMigrations()
+    {
+        logger.LogInformation("Applying database migrations...");
+        await dbContext.Database.MigrateAsync();
+        logger.LogInformation("Database migrations applied successfully");
+    }
+}
