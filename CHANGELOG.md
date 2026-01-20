d225019a9fdf274f5a078605c37484317b2780f1
Bjørnar Prytz <bjornar.groholt.prytz@arbeidstilsynet.no>
2026-01-20 12:19:10 +0100

    more docs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,7 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 
 - added(hexarch): added more doc strings to the public classes
-- added(hexarch): added deadline to the tilsynssak 
+- added(hexarch): added deadline to the tilsynssak (example domain)
 
 ## 2.2.5
 
--- templates/hexarch/API/API.Ports/ITilsynssakService.cs
+++ templates/hexarch/API/API.Ports/ITilsynssakService.cs
@@ -5,14 +5,45 @@ namespace Arbeidstilsynet.HexagonalArchitectureTemplateDocker.API.Ports;
 
 public interface ITilsynssakService
 {
+    /// <summary>
+    /// Creates a new Tilsynssak based on the provided DTO. The deadline is set based on the domain configuration.
+    /// </summary>
+    /// <param name="tilsynssakDto"></param>
+    /// <returns></returns>
     public Task<Tilsynssak> CreateNewSak(CreateTilsynssakDto tilsynssakDto);
-
+    
+    /// <summary>
+    /// Starts the case with the given <paramref name="sakId"/>, changing its status to <see cref="SakStatus.InProgress"/>.
+    /// </summary>
+    /// <param name="sakId"></param>
+    /// <returns></returns>
     public Task<Tilsynssak> StartSak(Guid sakId);
 
+    
+    /// <summary>
+    /// Ends the case with the given <paramref name="sakId"/>, changing its status to <see cref="SakStatus.Done"/>.
+    /// </summary>
+    /// <param name="sakId"></param>
+    /// <returns></returns>
     public Task<Tilsynssak> EndSak(Guid sakId);
+    
+    /// <summary>
+    /// Archives the case with the given <paramref name="sakId"/>, changing its status to <see cref="SakStatus.Archived"/>.
+    /// </summary>
+    /// <param name="sakId"></param>
+    /// <returns></returns>
     public Task<Tilsynssak> ArchiveSak(Guid sakId);
 
+    /// <summary>
+    /// Retrieves all Tilsynssak cases.
+    /// </summary>
+    /// <returns></returns>
     public Task<IEnumerable<Tilsynssak>> GetAllSaker();
 
+    /// <summary>
+    /// Retrieves a Tilsynssak by its unique identifier.
+    /// </summary>
+    /// <param name="sakId"></param>
+    /// <returns></returns>
     public Task<Tilsynssak> GetSakById(Guid sakId);
 }
